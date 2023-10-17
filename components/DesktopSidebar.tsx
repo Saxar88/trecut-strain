e954ef7f77b4be81fbd1f35651661c6b658939b6
Saxar88 <v.sasaran@protonmail.com>
2023-10-17 08:06:07 +0300

    build DesktopSidebar component

---
+++ components/DesktopSidebar.tsx
@@ -0,0 +1,42 @@
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+import { BiSearch } from "react-icons/bi";
+import { HiHome } from "react-icons/hi";
+
+import Box from "./Box";
+import Library from "./Library";
+import SidebarItem from "./SidebarItem";
+
+const DesktopSidebar = () => {
+	const pathname = usePathname();
+
+	const routes = useMemo(
+		() => [
+			{ icon: HiHome, label: "Acasă", active: pathname !== "caută", href: "/" },
+			{
+				icon: BiSearch,
+				label: "Caută",
+				active: pathname === "caută",
+				href: "/caută",
+			},
+		],
+		[pathname]
+	);
+
+	return (
+		<div className="hidden md:flex flex-col h-full w-80 p-2 gap-y-2 text-[#c8bea6]">
+			<Box>
+				<div className="flex flex-col gap-y-4 px-5 py-4">
+					{routes.map((item) => (
+						<SidebarItem key={item.label} {...item} />
+					))}
+				</div>
+			</Box>
+			<Box className="h-full overflow-y-auto">
+				<Library />
+			</Box>
+		</div>
+	);
+};
+
+export default DesktopSidebar;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,48 +1,15 @@
 "use client";
 
-import { usePathname } from "next/navigation";
-import { useMemo } from "react";
-import { BiSearch } from "react-icons/bi";
-import { HiHome } from "react-icons/hi";
-
-import Box from "./Box";
-import Library from "./Library";
-import SidebarItem from "./SidebarItem";
+import DesktopSidebar from "./DesktopSidebar";
 
 interface SidebarProps {
 	children: React.ReactNode;
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ children }) => {
-	const pathname = usePathname();
-
-	const routes = useMemo(
-		() => [
-			{ icon: HiHome, label: "Acasă", active: pathname !== "caută", href: "/" },
-			{
-				icon: BiSearch,
-				label: "Caută",
-				active: pathname === "caută",
-				href: "/caută",
-			},
-		],
-		[pathname]
-	);
-
 	return (
 		<div className="flex h-full">
-			<div className="hidden md:flex flex-col h-full w-80 p-2 gap-y-2 text-[#c8bea6]">
-				<Box>
-					<div className="flex flex-col gap-y-4 px-5 py-4">
-						{routes.map((item) => (
-							<SidebarItem key={item.label} {...item} />
-						))}
-					</div>
-				</Box>
-				<Box className="h-full overflow-y-auto">
-					<Library />
-				</Box>
-			</div>
+			<DesktopSidebar />
 			<main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
 		</div>
 	);
